8333433b558752bddb242f971d4dd2afc2432fb8
Lowe54 <jameslowe241@gmail.com>
2019-02-12 21:50:49 +0000

    Massive update to graphs.js, new graphs created by button functionality implemented, reset chart button functionality implemented. stying applied to button layout

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -4,12 +4,12 @@ queue()
 .await(makeGraphs)
 
 function makeGraphs(error, data){
-    console.log(data);
     var ndx = crossfilter(data);
-    chart_Regions(ndx);      
+    
+    chart_Regions(ndx);
+    
+    createDropdown(ndx);
     $(window).on('resize', function(e){
-        console.log("Window Resized");
-        
         var width = $('#content').innerWidth();
         if(width <= '768')
         {   
@@ -34,6 +34,14 @@ function makeGraphs(error, data){
         $('#regionCountBarChart').show();
         $('#regionCountRowChart').hide();
     }
+
+    dc.renderAll();
+    $('#Selector').after("<button class=\"regSelect btn btn-danger\" onClick=\"resetChart()\">RESET</button>");
+    $('.dc-select-option').each(function(e){
+        $('#Selector').after("<button class=\"regSelect btn btn-primary\" value=\""+$(this).val()+"\" onClick=\"filterRowChart('"+$(this).val()+"')\">"+$(this).val().toUpperCase()+"</button>");
+    });
+    $('button.regSelect').wrapAll('<div id="button_container"/>');
+    $('#Selector').hide();
  }
 
 //Select Dropdown
@@ -43,17 +51,17 @@ function createDropdown(ndx){
 
     dc.selectMenu('#Selector')
         .dimension(dim)
-        .group(group);
+        .group(group)
+        
     
 }
-
-
 //Charts
  function chart_Regions(ndx){
     var dim = ndx.dimension(dc.pluck('Region'));
     var group = dim.group();
-    dc.barChart('#regionCountBarChart')
-    .width(1250)
+
+   dc.barChart('#regionCountBarChart')
+    .width(1400)
     .height(500)
     .margins({top:10, right:50, bottom:30, left:50})
     .dimension(dim)
@@ -64,14 +72,117 @@ function createDropdown(ndx){
     .elasticY(true)
     .xAxisLabel("Region")
     .yAxis().ticks(20);
-
+    
+       
+   
 dc.rowChart('#regionCountRowChart')
     .width(500)
     .height(500)
     .dimension(dim)
     .group(group)
     .elasticX(true);
-    createDropdown(ndx);
-    dc.renderAll();   
+
+
+    resetChart = function(){
+        var dim = ndx.dimension(dc.pluck('Region'));
+        group = dim.group();
+        dc.barChart('#regionCountBarChart')
+        .width(1400)
+        .height(500)
+        .margins({top:10, right:50, bottom:30, left:50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Region")
+        .yAxis().ticks(20);
+
+        dc.rowChart('#regionCountRowChart')
+        .width(500)
+        .height(500)
+        .dimension(dim)
+        .group(group)
+        .elasticX(true)
+        $('#regionCountRowChart').html("");
+        $('#regionCountBarChart').html("");
+
+        dc.renderAll();
+    }
+
+    filterRowChart = function (value) {
+        console.log(value);
+        var dim = ndx.dimension(dc.pluck('ServiceTypes'))
+        group = reduceByRegion(dim,value);
+        STBar = dc.barChart('#regionCountBarChart')
+        .width(1400)
+        .height(500)
+        .margins({top:10, right:50, bottom:30, left:50})
+        .dimension(dim)
+        .group(group)
+        .valueAccessor(function(d){
+            if(d.value.match > 0 ){
+                return d.value.match;
+            } else{
+                return 0;
+            }
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Service Type")
+        .yAxis().ticks(20);
+
+       
+        dc.rowChart('#regionCountRowChart')
+        .width(500)
+        .height(500)
+        .dimension(dim)
+        .group(group)
+        .valueAccessor(function(d){
+            if(d.value.match > 0 ){
+                return d.value.match;
+            } else{
+                return 0;
+            }
+        })
+        .elasticX(true);
+       
+        $('#regionCountRowChart').html("");
+        dc.renderAll();
+        $('#regionCountBarChart g.x text').each(function(e){
+            $(this).css('transform', 'rotate(270deg) translateX(130px)').css('color', '#FFF');
+        })
+    };
+    
+       
  
  }
+ 
+ function reduceByRegion(dimension,rank){
+     console.log(rank);
+    return dimension.group().reduce(
+         function(p,v){
+             
+             p.total++;
+             if(v.Region == rank){
+                 p.match++
+             }
+             return p;
+         },
+         function (p,v){
+             p.total--;
+             if(v.Region == rank){
+                 p.match--
+             }
+             return p;
+         },
+         function (){
+             return{total:0, match:0}
+         }
+     );
+ }
+
+ 
