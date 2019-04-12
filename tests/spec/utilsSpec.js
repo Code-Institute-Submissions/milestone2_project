9b22dccb32b7309f9089ab3f73e5fd2d33344d2a
James Lowe <35152800+Lowe54@users.noreply.github.com>
2019-04-12 12:28:46 +0100

    Updated UtilsSpec.js
    Added extra tests covering the length of the array after the string is split, also testing if the string does contain the alphabet

--- tests/spec/utilsSpec.js
+++ tests/spec/utilsSpec.js
@@ -1,16 +1,37 @@
 describe('Utils.js', function() {
     beforeEach(function(){
         numCheck = new checkHeights();
+        letarray = new lettersplit();
+    });
+    it("Should exist", function(){
+        expect(numCheck).toBeDefined();
+        expect(letarray).toBeDefined();
     });
     describe('Should be able to check and compare heights of elements', function(){
-        it("Should exist", function(){
-            
-        });
+        
         it("should return 42", function(){
             numCheck.checkHeight(42,40);
             expect(numCheck.value).toBe(42);
         });
+        it("should return 28", function(){
+            numCheck.checkHeight(18,28);
+            expect(numCheck.value).toBe(28);
+        });
         
         
-    }); 
+    });
+    
+    describe('Should contain a string containing the alphabet', function(){
+        it('that should contain "abcdefghijklmnopqrstuvwxyz"', function(){
+            expect(letarray.string).toBe('abcdefghijklmnopqrstuvwxyz')
+        });
+        it(' and should then split this into an array which should exist', function(){
+            expect(letarray.stringsplit).toBeDefined();
+        });
+        it(' and have a length of 26', function(){
+            letarray.findlength('abcdefghijklmnopqrstuvwxyz');
+            expect(letarray.count).toBe(26);
+        })
+    });
+
 });
